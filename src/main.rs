0eff8e05c2540e0b5ad1f726a8bb2787591077ce
ULU <nilsien2001@gmail.com>
2024-03-20 17:24:37 +0000

    fix linting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use postgres::{Client, NoTls};
+use tokio_postgres::{Client, NoTls};
 use std::env;
+use ua_rlib::models::whisky::Whiskey;
 
 use eyre::{Context, Result};
 use request::get_all_whiskies;
@@ -10,28 +11,40 @@ mod utils;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let db_password =
+ let db_password =
         env::var("DB_PASSWORD").wrap_err("Failed finding enviroment variable `DB_PASSWORD`")?;
-    let mut client = Client::connect(
-        &format!("host=localhost:5432 database=ulu_prod user=ulu_backend password={db_password}"),
-        NoTls,
-    )?;
+let (client, connection) =
+        tokio_postgres::connect(&format!("host=localhost dbname=ulu_prod user=ulu_backend password={db_password}"), NoTls).await?;
 
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    
+    let mut i: i64 = 100;
     for w in get_all_whiskies().await? {
-        client
+   
+
+client
             .execute(
-                "INSERT INTO whisky (img, title, price, summary, volume, percentage) VALUES ($1, $2, $3, $4, $5, $6)",
+                "INSERT INTO whiskey (id, img, title, price, summary, volume, percentage) VALUES ($1, $2, $3, $4, $5, $6, $7)",
                 &[
+                    &i,
                     &w.img,
                     &w.title,
-                    &w.price,
+                    &(w.price as f64),
                     &w.summary,
-                    &w.volume,
-                    &(w.percentage as i64),
+                    &(w.volume as f64),
+                    &(w.percentage as f64),
                 ],
             )
+            .await
             .wrap_err("Failed writing to db")?;
+        i += 1;
     }
-
     Ok(())
 }
+
