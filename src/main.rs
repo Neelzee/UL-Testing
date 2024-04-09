e2ed38026aa02f59ca5c84ee786dcda57bdfe213
Nils Michael Fitjar <nilsien2001@gmail.com>
2024-04-09 11:39:15 +0200

    fixed summary

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio_postgres::{Client, NoTls};
 use std::env;
+use tokio_postgres::{Client, NoTls};
 use ua_rlib::models::whisky::Whiskey;
 
 use eyre::{Context, Result};
@@ -11,10 +11,13 @@ mod utils;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
- let db_password =
+    let db_password =
         env::var("DB_PASSWORD").wrap_err("Failed finding enviroment variable `DB_PASSWORD`")?;
-let (client, connection) =
-        tokio_postgres::connect(&format!("host=localhost dbname=ulu_prod user=ulu_backend password={db_password}"), NoTls).await?;
+    let (client, connection) = tokio_postgres::connect(
+        &format!("host=localhost dbname=ulu_prod user=ulu_backend password={db_password}"),
+        NoTls,
+    )
+    .await?;
 
     // The connection object performs the actual communication with the database,
     // so spawn it off to run on its own.
@@ -23,12 +26,10 @@ let (client, connection) =
             eprintln!("connection error: {}", e);
         }
     });
-    
+
     let mut i: i64 = 100;
     for w in get_all_whiskies().await? {
-   
-
-client
+        client
             .execute(
                 "INSERT INTO whiskey (id, img, title, price, summary, volume, percentage, avg_score) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
                 &[
@@ -48,4 +49,3 @@ client
     }
     Ok(())
 }
-
